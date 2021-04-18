39dddfd61d0cfbfb1a087f81d2d16a989ce5726d
acdigital-git <admin@acdigital-web.com>
2021-04-18 05:56:11 -0400

    Create simple animation view

--- lib/src/views/animation/simple_animation_view.dart
+++ lib/src/views/animation/simple_animation_view.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_hooks_app/src/widgets/base_widget.dart';
 
-class SimpleAnimationView extends StatefulWidget {
+class SimpleAnimationView extends HookWidget {
   SimpleAnimationView({Key? key}) : super(key: key);
 
-  @override
-  _SimpleAnimationViewState createState() => _SimpleAnimationViewState();
-}
-
-class _SimpleAnimationViewState extends State<SimpleAnimationView> {
   @override
   Widget build(BuildContext context) {
     return BaseWidget(
         appBar: AppBar(title: Text('Simple Animation')),
         child: Center(
-            child: Container(width: 200, height: 200, color: Colors.teal)));
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            IconButton(
+                icon: Icon(Icons.remove_rounded),
+                onPressed: () {},
+                iconSize: 40.0),
+            const SizedBox(width: 8.0),
+            TweenAnimationBuilder<int>(
+                duration: const Duration(seconds: 3),
+                tween: IntTween(begin: 0, end: 100),
+                builder: (_, int value, __) {
+                  return Text(value.toString(),
+                      style: TextStyle(fontSize: 40.0));
+                }),
+            const SizedBox(width: 8.0),
+            IconButton(
+                icon: Icon(Icons.add_rounded),
+                onPressed: () {},
+                iconSize: 40.0),
+          ]),
+        ])));
   }
 }
